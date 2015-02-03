4fa79d455a36647d7ee69ad277395b93fb06ecbe
zell.christopher <zelldon91@gmail.com>
2015-02-03 08:58:39 +0100

    removed some lines

--- Moccha-Android/src/de/tel/moccha/activities/fragments/CanteenFragment.java
+++ Moccha-Android/src/de/tel/moccha/activities/fragments/CanteenFragment.java
@@ -160,8 +160,6 @@ public class CanteenFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
           Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_canteen, container, false);
-
-//    getActivity().setTitle(c.getTitle());
     return rootView;
   }
 
