c16fbcc5a9d9ee5019859e317619e3a646b9632d
zell.christopher <zelldon91@gmail.com>
2015-04-13 15:21:45 +0200

    Creates a course list adapter to show the items in a list view

---
+++ Moccha-Android/src/de/tel/moccha/activities/fragments/adapters/course/CourseListAdapter.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 Quality and Usability Lab, Telekom Innvation Laboratories, TU Berlin..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tel.moccha.activities.fragments.adapters.course;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+import de.tel.moccha.activities.R;
+import de.tel.moccha.entities.course.Course;
+import de.tel.moccha.util.CourseComparator;
+import de.zell.android.util.EntityComparator;
+import de.zell.android.util.adapters.EntityListAdapter;
+import de.zell.android.util.db.Entity;
+
+/**
+ * The entity list adapter to show the universities in the university list.
+ * 
+ * @author Christopher Zell <zelldon91@googlemail.com>
+ */
+public class CourseListAdapter extends EntityListAdapter {
+
+  public CourseListAdapter(Context c) {
+    super(c);
+  }
+  
+  @Override
+  protected EntityComparator getComparator() {
+    return new CourseComparator();
+  }
+
+  @Override
+  protected void setEntityView(View row, int pos) {
+    Course course = (Course) entities.get(pos);
+    if (course != null) {
+      TextView title = (TextView) row.findViewById(de.zell.android.util.R.id.entity_title);
+      title.setText(course.getTitle());
+      title.setVisibility(View.VISIBLE);
+      
+      TextView descrp = (TextView) row.findViewById(de.zell.android.util.R.id.entity_description);
+      descrp.setText(course.getKindOfEvent());
+      descrp.setVisibility(View.VISIBLE);
+    }
+  }
+
+  @Override
+  protected String getSection(Entity e) {
+    return context.getString(R.string.course_list_section_header);
+  }
+  
+}
