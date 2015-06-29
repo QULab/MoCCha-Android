fbd048f3a14161b300597b00862f6302dd24f6d8
zell.christopher <zelldon91@gmail.com>
2015-06-29 21:12:40 +0200

    #5 adjust the eventlist frg
     * added load method
     * added on click method impl

--- Moccha-Android/src/de/tel/moccha/activities/fragments/event/EventListFragment.java
+++ Moccha-Android/src/de/tel/moccha/activities/fragments/event/EventListFragment.java
@@ -15,10 +15,85 @@
  */
 package de.tel.moccha.activities.fragments.event;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import de.tel.moccha.activities.fragments.CategoryListFragment;
+import de.tel.moccha.activities.fragments.adapters.event.EventListAdapter;
+import de.tel.moccha.activities.fragments.course.CourseFragment;
+import de.tel.moccha.entities.event.Event;
+import de.tel.moccha.entities.event.Events;
+import de.zell.android.util.activities.MainNavigationActivity;
+import de.zell.android.util.adapters.EntityListAdapter;
+import de.zell.android.util.async.AsyncGETRequester;
+import de.zell.android.util.async.GetRequestInfo;
+import de.zell.android.util.db.Entity;
+import de.zell.android.util.fragments.ActionBarTitleManager;
+import de.zell.android.util.fragments.EntityFragment;
+import de.zell.android.util.fragments.EntityListFragment;
+import de.zell.android.util.fragments.FragmentReplacer;
+import de.zell.android.util.json.JSONUnmarshaller;
+import java.util.ArrayList;
+import org.json.JSONObject;
+
 /**
  *
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
-public class EventListFragment {
+public class EventListFragment extends EntityListFragment {
+
+  
+  @Override
+  protected void onEntityClick(Entity e) {
+    Event event = (Event) e;
+    Bundle b = new Bundle();
+    b.putString(EntityFragment.ARG_ENTITY_URL, event.getUri());
+    b.putString(ActionBarTitleManager.ARG_ACTION_BAR_TITLE, event.getTitle());
+    Fragment frg = new EventFragment();
+    frg.setArguments(b);
+    FragmentReplacer.replace(getActivity().getSupportFragmentManager(),
+                             frg,
+                             FragmentReplacer.MAIN_CONTENT);
+  }
+
+  @Override
+  protected void onSectionClick(Object o) {
+    //do nothing
+  }
+
+  @Override
+  protected void loadEntities() {
+    AsyncGETRequester request = new AsyncGETRequester(new AsyncGETRequester.PostExecuteJob() {
+      public void doJob(JSONObject response) {
+        if (entities == null)
+          entities = new ArrayList<Entity>();
+        
+        Events events = JSONUnmarshaller.unmarshall(response, Events.class);
+        entities.addAll(events.getEvents());
+        
+        EntityListAdapter adapter = (EntityListAdapter) getListAdapter();
+        adapter.setEntities(entities);
+        adapter.notifyDataSetChanged();
+      }
+
+      public void doExeptionHandling(Throwable t) {
+        if (t != null)
+          Log.e(CategoryListFragment.class.getName(), t.getMessage(), t);
+      }
+
+      public void handleNewEtag(String url, String newEtag) {
+        //TODO
+      }
+    });
+    request.showProgress(((MainNavigationActivity) getActivity()).getProgressBar());
+    GetRequestInfo info = new GetRequestInfo(getURL(), null);
+    request.execute(info);
+  }
+
+  @Override
+  protected EntityListAdapter getEntityListAdapter(Context ctx) {
+    return new EventListAdapter(ctx);
+  }
   
 }
