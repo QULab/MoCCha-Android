1dc97f2dd5aaf8d71bfc5a6c052487049c7c7c6e
zell.christopher <zelldon91@gmail.com>
2015-06-29 20:55:07 +0200

    #5 addedevent comparator

---
+++ Moccha-Android/src/de/tel/moccha/util/EventComparator.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Quality and Usability Lab, Telekom Innvation Laboratories, TU Berlin..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tel.moccha.util;
+
+import de.tel.moccha.entities.event.Event;
+import de.zell.android.util.EntityComparator;
+import de.zell.android.util.db.Entity;
+
+/**
+ *
+ * @author Christopher Zell <zelldon91@googlemail.com>
+ */
+public class EventComparator extends EntityComparator {
+
+  @Override
+  public int compare(Entity arg0, Entity arg1) {
+    return ((Event) arg0).getTitle().compareTo(((Event) arg1).getTitle());
+  }
+}
