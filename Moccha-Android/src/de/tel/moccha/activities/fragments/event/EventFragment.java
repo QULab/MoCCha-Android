8db66d4225f8408f5526b4d5e8bffa95388b4169
zell.christopher <zelldon91@gmail.com>
2015-06-29 21:13:11 +0200

    #5 added event frgmt for event details
     * shows the information of the event

---
+++ Moccha-Android/src/de/tel/moccha/activities/fragments/event/EventFragment.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2015 Quality and Usability Lab, Telekom Innvation Laboratories, TU Berlin..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tel.moccha.activities.fragments.event;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import de.tel.moccha.activities.R;
+import de.tel.moccha.activities.fragments.course.CourseFragment;
+import de.tel.moccha.entities.course.Course;
+import de.tel.moccha.entities.event.Event;
+import de.zell.android.util.activities.MainNavigationActivity;
+import de.zell.android.util.async.AsyncGETRequester;
+import de.zell.android.util.async.GetRequestInfo;
+import de.zell.android.util.db.Entity;
+import de.zell.android.util.fragments.EntityFragment;
+import de.zell.android.util.json.JSONUnmarshaller;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Christopher Zell <zelldon91@googlemail.com>
+ */
+public class EventFragment extends EntityFragment {
+
+  @Override
+  protected void showEntity(Entity entity) {
+    Event event = (Event) entity;
+    if (event == null) {
+      return;
+    }
+
+    View root = this.getView();
+    if (root == null) {
+      return;
+    }
+
+    getTextView(root, R.id.event_header_info)
+            .setText(event.getTitle());
+    getTextView(root, R.id.event_description)
+            .setText(event.getDescription());
+    getTextView(root, R.id.event_organizer)
+            .setText(event.getOrganizer());
+    
+    getTextView(root, R.id.event_start)
+            .setText(event.getStart());
+    getTextView(root, R.id.event_period)
+            .setText(event.getPeriod());
+  }
+
+  @Override
+  protected void restoreInstance(Bundle values) {
+    //do nothing
+  }
+
+  @Override
+  protected void loadEntity() {
+    AsyncGETRequester request = new AsyncGETRequester(new AsyncGETRequester.PostExecuteJob() {
+      public void doJob(JSONObject response) {
+        entity = JSONUnmarshaller.unmarshall(response, Event.class);
+        showEntity(entity);
+      }
+
+      public void doExeptionHandling(Throwable t) {
+        Log.e(CourseFragment.class.getName(), t.getMessage(), t);
+      }
+
+      public void handleNewEtag(String url, String newEtag) {
+        //TODO
+      }
+    });
+    request.showProgress(((MainNavigationActivity) getActivity()).getProgressBar());
+    GetRequestInfo info = new GetRequestInfo(getURL(), null);
+    request.execute(info);
+  }
+
+  @Override
+  protected void postRestore() {
+    //do nothing
+  }
+  
+  
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.fragment_event, container, false);
+    return rootView;
+  }
+
+  
+}
