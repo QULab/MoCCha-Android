e7ea8bac966cec801009a8c2121ef3605a3f979a
zell.christopher <zelldon91@gmail.com>
2015-07-07 14:02:54 +0200

    #10 added zoom to map
     * added zooming level to the map in the info dialog

--- Moccha-Android/src/de/tel/moccha/activities/fragments/canteen/CanteenInfoDialogFragment.java
+++ Moccha-Android/src/de/tel/moccha/activities/fragments/canteen/CanteenInfoDialogFragment.java
@@ -23,49 +23,53 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import de.tel.moccha.activities.R;
 import de.tel.moccha.entities.canteen.Canteen;
 import de.tel.moccha.util.StringFormatter;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Represents the class for a info dialog fragment.
- * 
+ *
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public class CanteenInfoDialogFragment extends DialogFragment implements OnMapReadyCallback {
-  
+
   /**
    * The argument key for the canteen as argument.
    */
   public static final String ARG_CANTEEN_INFO = "canteen.info";
-  
+
   /**
    * The tag key to save the canteen information.
    */
   private static final String TAG_INFO_CANTEEN = "tag.info";
-  
+
   /**
    * The canteen which contains the information which should be shown.
    */
   private Canteen canteen;
-  
+
   /**
    * The formatter which will be used to format the strings given by id.
    */
   private StringFormatter formatter;
-  
+
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
     formatter = new StringFormatter(getActivity());
-    
+
     if (savedInstanceState == null) {
       savedInstanceState = getArguments();
     }
-    
+
     if (savedInstanceState != null) {
       canteen = (Canteen) savedInstanceState.getSerializable(ARG_CANTEEN_INFO);
     }
@@ -75,35 +79,33 @@ public class CanteenInfoDialogFragment extends DialogFragment implements OnMapRe
     setDialogContent(v);
     setOnPositiveClickListener(v);
     setOnNegativeClickListener(v);
-    
-    
-    SupportMapFragment mapFragment =
-            (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.canteen_dialog_map);
+
+    SupportMapFragment mapFragment
+            = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.canteen_dialog_map);
     mapFragment.getMapAsync(this);
-    
+
     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
     return builder.setView(v).create();
   }
-  
-  
+
   @Override
   public void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
     outState.putSerializable(TAG_INFO_CANTEEN, canteen);
   }
-  
+
   /**
    * Returns the dialog layout id which should be shown by the dialog fragment.
-   * 
+   *
    * @return the id of the layout
    */
   protected int getDialogLayout() {
     return R.layout.canteen_info_dialog;
   }
-  
+
   /**
    * Places the content into the root view.
-   * 
+   *
    * @param root the root view
    */
   protected void setDialogContent(View root) {
@@ -116,14 +118,15 @@ public class CanteenInfoDialogFragment extends DialogFragment implements OnMapRe
               .setText(formatter.getFormatedHTMLString(R.string.canteen_phone, canteen.getPhone()));
       getTextView(root, R.id.canteen_opening_hours)
               .setText(formatter.getFormatedHTMLString(R.string.canteen_opening_hours, canteen.getOpeningHours()));
-      
+
       TextView header = (TextView) root.findViewById(R.id.alert_info_header);
       header.setText(canteen.getTitle());
     }
   }
-  
+
   /**
    * Sets the positive click listener for the dialog fragment.
+   *
    * @param root the root view
    */
   protected void setOnPositiveClickListener(View root) {
@@ -135,37 +138,41 @@ public class CanteenInfoDialogFragment extends DialogFragment implements OnMapRe
       }
     });
   }
-  
+
   /**
    * Sets the negative click listener for the dialog fragment.
+   *
    * @param root the root view
    */
   protected void setOnNegativeClickListener(View root) {
-    
+
   }
-  
+
   /**
    * Returns the text view for the given id from the root view.
-   * 
+   *
    * @param root the root view
    * @param id the id which identifies the view
    * @return the corresponding view
    */
   private TextView getTextView(View root, int id) {
     return ((TextView) root.findViewById(id));
-  } 
-  
-  
-     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     */
-    @Override
-    public void onMapReady(GoogleMap map) {
-      if (canteen != null)
-        map.addMarker(new MarkerOptions()
-                            .position(new LatLng(canteen.getLatitude(),
-                                                  canteen.getLongitude()))
-                            .title(canteen.getTitle()));
+  }
+
+  @Override
+  public void onMapReady(GoogleMap map) {
+    if (canteen != null) {
+      LatLng latlng = new LatLng(canteen.getLatitude(), canteen.getLongitude());
+      map.addMarker(new MarkerOptions()
+                          .position(latlng)
+                          .title(canteen.getTitle()));
+
+      CameraPosition cameraPosition = new CameraPosition.Builder()
+                                                        .target(latlng)
+                                                        .zoom(14.0f)
+                                                        .build();
+      CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+      map.moveCamera(cameraUpdate);
     }
+  }
 }
